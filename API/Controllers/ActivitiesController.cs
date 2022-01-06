760588c282a85723bd63a3489e3c671f54e7a3ed
devlopment.ivan <dev.ivanifotech@gmail.com>
2022-01-06 22:40:05 +0530

    second module commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await context.Activities.FindAsync(id);
+        }
+    }
+}
---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+       
+    }
+}
